daa6b87e32ff6690edfc02b8859621744c7108c1
Rodrigo Lopez <0262146@up.edu.mx>
2024-09-28 18:23:13 -0600

    I think I'm Done with The fifth Portfolio

--- Portfolio/05-JS/js/pig_latin.js
+++ Portfolio/05-JS/js/pig_latin.js
@@ -4,30 +4,41 @@ Pig Latin
 
 function igpayAtinlay(str) {
   // TODO: Initialize the word array properly
-  var returnArray = [],
-    wordArray = [];
+  var returnArray = [];
+  var wordArray = str.split(" ");
   // TODO: make sure that the output is being properly built to produce the desired result.
   for (var i = 0; i < wordArray.length; i++) {
     var word = wordArray[i];
+    var end = "";
     var beginning = word.charAt(0);
 
     if (/[aeiouAEIOU]/.test(beginning)) {
-      returnArray.push(word);
+      returnArray.push(word + "way");
       continue;
     }
 
     for (var ii = 1; ii < word.length; ii++) {
       if (/[aeiouAEIOU]/.test(word.charAt(ii))) {
+        end += word.slice(ii) + beginning + "ay";
         break;
       } else {
         beginning += word.charAt(ii);
       }
     }
+    returnArray.push(end);
   }
   return returnArray.join(" ");
 }
+
+
 
 // Some examples of expected outputs
 console.log(igpayAtinlay("pizza")); // "izzapay"
 console.log(igpayAtinlay("apple")); // "appleway"
 console.log(igpayAtinlay("happy meal")); // "appyhay ealmay"
+
+
+$("#btn").on('click', () =>{
+  var PigTxt = $("#txtVal").val();
+  $("#pigExtay").text(igpayAtinlay(PigTxt));
+})
--- Portfolio/05-JS/js/sieve_eratosthenes.js
+++ Portfolio/05-JS/js/sieve_eratosthenes.js
@@ -5,17 +5,44 @@
 
 // TODO: Adjust this script so it can work with the sieve.html file.
 
+
+
 var sieve = function (n) {
   "use strict";
 
-  var array = [],
-    primes = [],
-    i,
-    j;
+  var array = Array(n + 1).fill(true),
+      primes = [],
+      i,
+      j;
 
   // TODO: Implement the sieve of eratosthenes algorithm to find all the prime numbers under the given number.
 
+  array[0] = array[1] = false;
+  
+  for (i = 2; i <= Math.sqrt(n); i++) {
+    if (array[i]) {
+      for (j = i * i; j <= n; j += i) {
+        array[j] = false;
+      }
+    }
+  }
+
+  for (i = 2; i <= n; i++) {
+    if (array[i]) {
+      primes.push(i);
+    }
+  }
+
   return primes;
 };
+
+$('#btn').on('click', function () {
+  var num = parseInt($('#num').val());
+
+  var primeNumbers = sieve(num);
+    $('#primes').text(primeNumbers.join(", "));
+    
+});
+
 
 console.log(sieve(1000000));
