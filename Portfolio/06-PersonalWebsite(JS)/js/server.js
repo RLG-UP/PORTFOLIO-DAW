b6a97fd92ea367b19686312b8d8b15ac41f5fa07
Rodrigo Lopez <0262146@up.edu.mx>
2024-09-28 22:11:02 -0600

    Updating Portfolio 6 Listening to The Alan Parsons Project

--- Portfolio/06-PersonalWebsite(JS)/js/server.js
+++ Portfolio/06-PersonalWebsite(JS)/js/server.js
@@ -1,1 +1,32 @@
-console.log("Running the script");
+
+
+$(document).ready(()=>{
+    $("#addRow").click((e)=>{
+        e.preventDefault();
+        
+        var date = $("#insertForm #date").val();
+        var start = $("#insertForm #start").val();
+        var end = $("#insertForm #end").val();
+        var description = $("#insertForm #description").val();
+        var place = $("#insertForm #place").val();
+        var type = $("#insertForm #type").val();
+        var notes = $("#insertForm #notes").val();
+        var flag = $("#insertForm #color").val();
+        var availability = $("#insertForm input[name='availability']:checked").val();
+
+        var rowSchedule = "<tr>
+        <td>${date}</td>
+        <td>${start}</td>
+        <td>${end}</td>
+        <td>${description}</td>
+        <td>${place}</td>
+        <td>${type}</td>
+        <td>${notes}</td>
+        <td style="background-color:${flag};"></td>
+        <td>${availability ? `<img src="Icons/busy.png" alt="Not Available" id="IconPhoto">` : `<img src="Icons/available.png" alt="Available" id="IconPhoto">`}</td>
+      </tr>";
+
+        $('#agendaTable tbody').append(rowSchedule);
+        $("#insertForm")[0].reset();
+    });
+});
