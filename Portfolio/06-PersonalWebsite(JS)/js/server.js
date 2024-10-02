a81a7723a6cfc453f858f53e8defd9885ae1e42c
Rodrigo Lopez <0262146@up.edu.mx>
2024-10-02 08:29:40 -0600

    Making a commit to the JS Personal webpage

--- Portfolio/06-PersonalWebsite(JS)/js/server.js
+++ Portfolio/06-PersonalWebsite(JS)/js/server.js
@@ -1,32 +1,35 @@
+$(document).ready(function () {
+    $('#addRow').click(function () {
+        // Get form values
+        var date = $('#date').val();
+        var start = $('#start').val();
+        var end = $('#end').val();
+        var description = $('#description').val();
+        var place = $('#place').val();
+        var type = $('#type').val();
+        var notes = $('#notes').val();
+        var flagColor = $('#color').val();
+        var availability = $('#availability').val();
 
+        // Create a new row
+        var newRow = `
+            <tr>
+                <td>${date}</td>
+                <td>${start}</td>
+                <td>${end}</td>
+                <td>${description}</td>
+                <td>${place}</td>
+                <td>${type}</td>
+                <td>${notes}</td>
+                <td style="background-color: ${flagColor};"></td>
+                <td>${availability === 'Busy' ? '<img src="Icons/busy.png" alt="Not Available" id="IconPhoto">' : '<img src="Icons/free.png" alt="Available" id="IconPhoto">'}</td>
+            </tr>
+        `;
 
-$(document).ready(()=>{
-    $("#addRow").click((e)=>{
-        e.preventDefault();
-        
-        var date = $("#insertForm #date").val();
-        var start = $("#insertForm #start").val();
-        var end = $("#insertForm #end").val();
-        var description = $("#insertForm #description").val();
-        var place = $("#insertForm #place").val();
-        var type = $("#insertForm #type").val();
-        var notes = $("#insertForm #notes").val();
-        var flag = $("#insertForm #color").val();
-        var availability = $("#insertForm input[name='availability']:checked").val();
+        // Append the new row to the table
+        $('#agendaTable tbody').append(newRow);
 
-        var rowSchedule = "<tr>
-        <td>${date}</td>
-        <td>${start}</td>
-        <td>${end}</td>
-        <td>${description}</td>
-        <td>${place}</td>
-        <td>${type}</td>
-        <td>${notes}</td>
-        <td style="background-color:${flag};"></td>
-        <td>${availability ? `<img src="Icons/busy.png" alt="Not Available" id="IconPhoto">` : `<img src="Icons/available.png" alt="Available" id="IconPhoto">`}</td>
-      </tr>";
-
-        $('#agendaTable tbody').append(rowSchedule);
-        $("#insertForm")[0].reset();
+        // Optionally, clear the input fields after adding the row
+        $('#insertForm')[0].reset();
     });
 });
