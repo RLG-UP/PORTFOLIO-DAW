a51d51edab6dcf9e20ad6390b0453212bf91d434
Rodrigo Lopez <0262146@up.edu.mx>
2024-10-04 17:32:38 -0600

    Commiting finished portfolio 6, the problem was the submit button

--- Portfolio/06-PersonalWebsite(JS)/js/server.js
+++ Portfolio/06-PersonalWebsite(JS)/js/server.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         var type = $('#type').val();
         var notes = $('#TxtArea').val();
         var flagColor = $('#color').val();
-        var availability = $('#busy').checked;
+        var availability = $('#busy').is(':checked'); 
 
 
 
@@ -24,13 +24,40 @@ $(document).ready(function () {
                 <td>${type}</td>
                 <td>${notes}</td>
                 <td style="background-color: ${flagColor};"></td>
-                <td>${availability === true ? '<img src="Icons/busy.png" alt="Not Available" id="IconPhoto">' : '<img src="Icons/free.png" alt="Available" id="IconPhoto">'}</td>
-            </tr>
+                <td>${availability ? '<img src="Icons/busy.png" alt="Not Available" id="IconPhoto">' : '<img src="Icons/free.png" alt="Available" id="IconPhoto">'}</td>
+        </tr>
         `;
 
         // Append the new row to the table
         $('#scheduleTable tbody').append(newRow);
 
     
+    });
+
+    $('#addTest').click(function(){
+
+        var date = "1";
+        var start = "2";
+        var end = "3";
+        var description = "4";
+        var place = "5";
+        var type = "6";
+        var notes = "7";
+        var flagColor = "8";
+        var availability = "9";
+
+        var insertar = `<tr>
+                <td>${date}</td>
+                <td>${start}</td>
+                <td>${end}</td>
+                <td>${description}</td>
+                <td>${place}</td>
+                <td>${type}</td>
+                <td>${notes}</td>
+                <td>${flagColor}</td>
+                <td>${availability}</td>
+            </tr>`;
+
+        $('#tablaPrueba tbody').append(insertar);
     });
 });
