58ef6ffb92f58c5811083f1b0bb94638bd6aa760
Rodrigo Lopez <0262146@up.edu.mx>
2024-10-04 18:20:36 -0600

    Commiting finished portfolio 07 i think, REMEMBER TO CHANGE THE type in the package.json file when running the different js files

---
+++ Portfolio/07-BasicNode/multCalls.js
@@ -0,0 +1,6 @@
+import {randomSuperhero} from 'superheroes';
+import {randomSupervillain} from 'supervillains';
+var superH = randomSuperhero();
+var superV = randomSupervillain();
+
+console.log(superH + " /VS/ " + superV);
