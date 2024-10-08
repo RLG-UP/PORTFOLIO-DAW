21ee2ba97f82fc2a30e60d7f515fc825d941e551
Rodrigo Lopez <0262146@up.edu.mx>
2024-10-08 08:50:51 -0600

    Changes to 09 portfolio listening to Kanye West

--- Portfolio/09-ExpressPersonalAPI/server.js
+++ Portfolio/09-ExpressPersonalAPI/server.js
@@ -9,10 +9,11 @@ app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
 var names = [];
+var tasks = [];
 
 app.route('/')
     .get((req,res)=>{
-        res.render('indx', {names});
+        res.render('indx', {names, tasks});
     });
 
 
@@ -23,12 +24,20 @@ app.post('/greet', (req, res)=>{
     console.log(nom);
     names.push(nom);
 
-    res.render('indx', { "names": names });
+    res.render('indx', { "names": names , tasks});
 });
 
 app.get('/sup', (req,res)=>{
-    var vals = req.body.vals;
-    res.render('wzzp', {"vals": vals})
+    var vals = req.query.vals;
+    res.render('wzzp', {"vals": vals});
+    console.log(vals);
+    
+});
+
+app.post('/task', (req, res)=>{
+    var tsk = req.body.tsk;
+    tasks.push(tsk);
+    res.render('indx', {"tasks": tasks, names});
 });
 
 app.listen(3000, ()=>{
