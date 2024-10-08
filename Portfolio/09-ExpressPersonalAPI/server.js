e0b148e5ab11bc2d8805b38fd67bf0a3a5aae1c5
Rodrigo Lopez <0262146@up.edu.mx>
2024-10-07 19:11:42 -0600

    Commiting more changes to 09 portfolio

--- Portfolio/09-ExpressPersonalAPI/server.js
+++ Portfolio/09-ExpressPersonalAPI/server.js
@@ -8,21 +8,28 @@ app.use(express.static("public"));
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
+var names = [];
+
 app.route('/')
     .get((req,res)=>{
-        res.render('indx');
+        res.render('indx', {names});
     });
 
 
-var names = [];
+
 
 app.post('/greet', (req, res)=>{
     var nom = req.body.name;
     console.log(nom);
-    res.render('indx', {"nom": nom});
+    names.push(nom);
+
+    res.render('indx', { "names": names });
 });
 
-
+app.get('/sup', (req,res)=>{
+    var vals = req.body.vals;
+    res.render('wzzp', {"vals": vals})
+});
 
 app.listen(3000, ()=>{
     console.log("Application listening port 3000");
