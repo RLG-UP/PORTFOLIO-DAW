606b505c415e67cfb4cf568107e64bbe4677fc9e
RLG-UP <0262146@up.edu.mx>
2024-10-23 18:44:20 -0600

    Uploading MAJOR changes to 12th portfolio, just discovered hidden character

--- Portfolio/12-ExpressMongoose/server.js
+++ Portfolio/12-ExpressMongoose/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const csv = require("csv-parser");
+const fs = require("fs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -64,18 +66,79 @@ let countries = [
   { code: "DEN", label: "Denmark" },
 ];
 
+var arrDrivers = []
+var arrTeams = []
 
+fs.createReadStream("public/data/f1_2023.csv")
+  .pipe(csv())
+  .on("data", async (row)=>{
+    console.log("Receiving information from the .csv file");
+    console.log("--> " + JSON.stringify(row));
+    const cleanedRow = {};
+    for (const key in row) {
+        const cleanedKey = key.replace(/^\s+|\s+$/g, '').replace(/[^\x20-\x7E]/g, '');
+        cleanedRow[cleanedKey] = row[key];
+    }
 
-app.get("/", (req, res) => {
-  res.render("index");
+    console.log("Cleaned row data:", cleanedRow); // Check the cleaned data
+
+    if (cleanedRow.number) {
+      const existNum = await Team.findOne({ id: cleanedRow.number });
+
+      if(!existNum){
+        console.log("Inserting new Team from the .csv file");
+        const team = new Team({
+            id: Number(cleanedRow.number),
+            name: cleanedRow.current_team,
+            nationality: cleanedRow.nationality,
+            url: cleanedRow.url,
+        });
+        await team.save();
+        console.log(team);
+
+        console.log("Inserting new Pilot from the .csv file");
+        const pilot = new Driver({
+            num: Number(cleanedRow.number),
+            code: cleanedRow.code,
+            forename: cleanedRow.forename,
+            surname: cleanedRow.surname,
+            dob: new Date(cleanedRow.dob.split("/").reverse().join("-")),
+            nationality: cleanedRow.nationality,
+            url: cleanedRow.url,
+            team: team,
+        });
+
+        await pilot.save();
+        console.log(pilot);
+      }
+        
+      
+    }else{
+      console.log("No number received");
+    }
+    
+  })
+  .on("end", ()=>{
+    console.log("Finished inserting data from the .csv file");
+  })
+
+app.get("/", async (req, res) => {
+  arrDrivers = await Driver.find({});
+  arrTeams = await Team.find({});
+
+  var params = {
+    arrDrivers,
+    arrTeams
+  };
+  res.render("index", params);
 });
 
 app.route("/add")
   .post(async (req, res)=>{
     const team = new Team({
       id: req.body.id,
-      name: req.body.name,
-      nationality: req.body.nationality,
+      name: req.body.team,
+      nationality: req.body.nation,
       url: req.body.url
     });
     await team.save();
@@ -84,12 +147,12 @@ app.route("/add")
     const pilot = new Driver({
       num: req.body.num,
       code: req.body.code,
-      forename: req.body.forename,
-      surname: req.body.surname,
+      forename: req.body.name,
+      surname: req.body.lname,
       dob: req.body.dob,
-      nationality: req.body.nationality,
+      nationality: req.body.nation,
       url: req.body.url,
-      team: team
+      team: team,
     });
 
     await pilot.save();
